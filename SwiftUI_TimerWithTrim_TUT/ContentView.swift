d562704b206eae1e7575c3e5bb1f77f1d7b62e3b
SeonJungKim <ksj083895@gmail.com>
2021-03-13 12:39:47 +0500

    trim() Shape의 일부 그리기 (Timer)

--- SwiftUI_TimerWithTrim_TUT/ContentView.swift
+++ SwiftUI_TimerWithTrim_TUT/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var completionAmount: CGFloat = 0.0
+    
+    //타이머 설정
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Circle()
+            .trim(from: 0, to: completionAmount)
+            .stroke(AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center),lineWidth: 20)
+            
+            .frame(width: 200, height: 200)
+            
+            //시계방향으로 돌아갈수 있도록
+            .rotationEffect(.degrees(-90))
+            
+            //타이머를 표현할때 onReceive는 필수이다.
+            .onReceive(timer) { _ in
+                withAnimation {
+                    if completionAmount == 1 {
+                        completionAmount = 0
+                    } else {
+                        completionAmount += 0.2
+                    }
+                }
+            }
     }
 }
 
