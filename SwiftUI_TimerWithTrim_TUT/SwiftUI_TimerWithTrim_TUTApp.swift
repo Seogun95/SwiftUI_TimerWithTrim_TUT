a03b203e863a500610c2e7b5b80e527a52914eee
SeonJungKim <ksj083895@gmail.com>
2021-03-13 12:38:40 +0500

    Initial Commit

---
+++ SwiftUI_TimerWithTrim_TUT/SwiftUI_TimerWithTrim_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TimerWithTrim_TUTApp.swift
+//  SwiftUI_TimerWithTrim_TUT
+//
+//  Created by 김선중 on 2021/03/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TimerWithTrim_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
